70e40e548ac0e7e93504d399b11b72b30813120e
Joshua Spence <josh@joshuaspence.com>
2021-08-21 13:17:48 +1000

    Add `name` attribute

--- custom_components/unavailable_entities/sensor.py
+++ custom_components/unavailable_entities/sensor.py
@@ -1,9 +1,21 @@
 from typing import Optional
 
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+import voluptuous as vol
+
+DEFAULT_NAME = "Unavailable Entities"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    }
+)
 
 
 def setup_platform(
@@ -12,8 +24,10 @@ def setup_platform(
     add_entities: AddEntitiesCallback,
     discovery_info: Optional[DiscoveryInfoType] = None,
 ) -> bool:
+    name = config.get(CONF_NAME)
+
     add_entities(
-        [UnavailableEntitiesSensor(hass)],
+        [UnavailableEntitiesSensor(hass, name)],
         update_before_add=True,
     )
 
@@ -21,8 +35,9 @@ def setup_platform(
 
 
 class UnavailableEntitiesSensor(Entity):
-    def __init__(self, hass: HomeAssistant) -> None:
+    def __init__(self, hass: HomeAssistant, name: Optional[str] = None) -> None:
         self.hass = hass
+        self._name = name
         self._state = None
 
     @property
@@ -35,6 +50,10 @@ class UnavailableEntitiesSensor(Entity):
             return "mdi:check-circle"
         else:
             return "mdi:alert-circle"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self._name
 
     @property
     def should_poll(self) -> bool:
--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -2,7 +2,10 @@ from homeassistant.components.sensor import DOMAIN
 from homeassistant.const import ATTR_ICON, ATTR_UNIT_OF_MEASUREMENT
 from homeassistant.setup import async_setup_component
 
-from custom_components.unavailable_entities.sensor import UnavailableEntitiesSensor
+from custom_components.unavailable_entities.sensor import (
+    DEFAULT_NAME,
+    UnavailableEntitiesSensor,
+)
 
 
 async def test_sensor(hass):
@@ -52,7 +55,29 @@ async def test_sensor_defaults(hass):
 
     state = hass.states.get("sensor.unavailable_entities")
     assert state.entity_id == "sensor.unavailable_entities"
-    assert state.name == "unavailable entities"
+    assert state.name == DEFAULT_NAME
     assert state.attributes.get(ATTR_ICON) == "mdi:check-circle"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) is None
-    assert state.state == "0"
+
+
+async def test_sensor_customizations(hass):
+    sensor_name = "Test Sensor"
+
+    assert await async_setup_component(
+        hass,
+        DOMAIN,
+        {
+            "sensor": {
+                "platform": "unavailable_entities",
+                "name": sensor_name,
+            },
+        },
+    )
+
+    await hass.async_block_till_done()
+    await hass.async_start()
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.unavailable_entities")
+    assert state.entity_id == "sensor.unavailable_entities"
+    assert state.name == sensor_name
